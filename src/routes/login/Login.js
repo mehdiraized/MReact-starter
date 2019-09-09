01a2d5e6a85898a218a50f96c7599025ebc8e034
mehdi <mehdiraized@gmail.com>
2019-09-10 04:11:41 +0430

    Add Formik

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Formik } from 'formik';
 import withStyles from 'isomorphic-style-loader/withStyles';
 import s from './Login.scss';
 
@@ -12,36 +13,82 @@ class Login extends React.Component {
           <p className={s.lead}>
             Log in with your username or company email address.
           </p>
-          <form method="post">
-            <div className={s.formGroup}>
-              <label className={s.label} htmlFor="usernameOrEmail">
-                Username or email address:
-                <input
-                  className={s.input}
-                  id="usernameOrEmail"
-                  type="text"
-                  name="usernameOrEmail"
-                  autoFocus // eslint-disable-line jsx-a11y/no-autofocus
-                />
-              </label>
-            </div>
-            <div className={s.formGroup}>
-              <label className={s.label} htmlFor="password">
-                Password:
-                <input
-                  className={s.input}
-                  id="password"
-                  type="password"
-                  name="password"
-                />
-              </label>
-            </div>
-            <div className={s.formGroup}>
-              <button className={s.button} type="submit">
-                Log in
-              </button>
-            </div>
-          </form>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <div className={s.formGroup}>
+                  <label className={s.label} htmlFor="usernameOrEmail">
+                    Username or email address:
+                    <input
+                      className={s.input}
+                      id="usernameOrEmail"
+                      type="email"
+                      name="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      autoFocus // eslint-disable-line jsx-a11y/no-autofocus
+                    />
+                    {errors.email && touched.email && <div>{errors.email}</div>}
+                  </label>
+                </div>
+                <div className={s.formGroup}>
+                  <label className={s.label} htmlFor="password">
+                    Password:
+                    <input
+                      className={s.input}
+                      id="password"
+                      type="password"
+                      name="password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}
+                    />
+                    {errors.password && touched.password && (
+                      <div>{errors.password}</div>
+                    )}
+                  </label>
+                </div>
+                <div className={s.formGroup}>
+                  <button
+                    className={s.button}
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Log in
+                  </button>
+                </div>
+              </form>
+            )}
+          </Formik>
         </div>
       </div>
     );
